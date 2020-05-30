3094f1d54cad3a8a9a16cb72020305d02de3d6b1
SashvDave <55955567+SashvDave@users.noreply.github.com>
2020-05-30 15:24:44 -0700

    Update login.dart

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -1,104 +1,133 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 
-class HomeScreen extends StatelessWidget {
-  final List<Supply> supplies = [
-    Supply(
-      itemDescription: '12 loaves of bread',
-      spotted: DateTime.now().subtract(Duration(minutes: 30)),
-      username: 'helpful_user',
-      address: '2323 Amazing Drive',
-      store: Store.walmart(location: 'Union Landing'),
-    ),
-  ];
+void main() {
+  runApp(MyApp());
+}
 
-  Widget buildItem(BuildContext context, int index) {
-    final Supply item = supplies[index];
-    final TextStyle titleStyle = Theme
-        .of(context)
-        .textTheme
-        .headline5;
-    final Duration fancyTime = DateTime.now().difference(item.spotted);
-
-    return Container(
-      margin: const EdgeInsets.only(bottom: 8),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Text(
-                '${item.itemDescription} at ${item.store.name}',
-                style: titleStyle,
-              ),
-              Text(
-                  '${item.itemDescription} are available at ${item.store}. '
-                      'Spotted $fancyTime ago by ${item.username}.'
-              ),
-            ],
-          ),
-        ),
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepOrangeAccent,
+        accentColor: Colors.deepOrangeAccent,
+        fontFamily: 'Georgia',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: MyHomePage(title: 'Login'),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Supplies'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            onPressed: () {},
-          ),
-        ],
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+        title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
-        child: ListView.builder(
-          itemBuilder: buildItem,
-          itemCount: supplies.length,
+      body: new Container(
+        child: new Column(
+          children: [
+            new Container(
+                height: 150.0,
+                width: 100.0,
+                child: FlutterLogo()
+            ),
+            new Container(
+              width: 370,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: new InputDecoration(
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
+                  ),
+                  hintText: 'Enter your email ID',
+                  labelText: 'Email',
+                  helperText: '(ex.user123@gmail.com)',
+                  contentPadding: new EdgeInsets.fromLTRB(20.0, 10.0, 100.0, 10.0),
+                ),
+              ),
+            ),
+            new Container (
+              width: 370,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: new InputDecoration(
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
+                  ),
+                  hintText: 'Enter your password here',
+                  labelText: 'Password',
+                  helperText: '(ex. user123)',
+                  contentPadding: new EdgeInsets.fromLTRB(20.0, 10.0, 100.0, 10.0),
+                ),
+              ),
+            ),
+            new Container(
+              height: 68,
+              width: 370,
+              padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () => {},
+                  textColor: Colors.black,
+                  color: Colors.deepOrangeAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: const EdgeInsets.all(8.0),
+                  child: new Text(
+                    "Login",
+                  ),
+                ),
+            ),
+            new Container(
+              height: 68,
+              width: 370,
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () => {},
+                textColor: Colors.black,
+                color: Colors.deepOrangeAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                padding: const EdgeInsets.all(8.0),
+                child: new Text(
+                  "Sign in with Google",
+                ),
+              ),
+            ),
+            new Container(
+              height: 68,
+              width: 370,
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () => {},
+                textColor: Colors.black,
+                color: Colors.deepOrangeAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                padding: const EdgeInsets.all(8.0),
+                child: new Text(
+                  "Don't have an account yet? Register!",
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-      bottomNavigationBar: BottomTabBar(
-        items: items,
-        selectedIndex: 1,
       ),
     );
   }
-}
-
-class Supply {
-  final String itemDescription;
-  final DateTime spotted;
-  final String username;
-  final String address;
-  final Store store;
-
-  Supply({@required this.itemDescription,
-    @required this.spotted,
-    @required this.username,
-    @required this.address,
-    @required this.store,
-  });
-}
-
-class Store {
-  final String name;
-  final String locationName;
-
-  Store._({@required this.name, @required this.locationName});
-
-  factory Store.walmart({@required String location})
-  => Store._(name: 'Walmart', locationName: location);
-
-  factory Store.target({@required String location})
-  => Store._(name: 'Target', locationName: location);
-
-  String toString() => '$locationName $name';
 }
