6c4863414c7ec3820eeaae496bf4e0f8efd512ef
SashvDave <55955567+SashvDave@users.noreply.github.com>
2020-05-31 01:21:21 -0700

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,8 +3,11 @@ import 'package:provider/provider.dart';
 import 'home_screen.dart';
 import 'login.dart';
 import 'register.dart';
-import 'auth.dart';
 import 'add.dart';
+import 'test.dart';
+import 'settings.dart';
+import 'auth.dart';
+
 
 class MyApp extends StatelessWidget {
   final BaseAuth auth;
@@ -21,20 +24,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.orange,
           accentColor: Colors.orangeAccent,
-          // This makes the visual density adapt to the platform that you run
-          // the app on. For desktop platforms, the controls will be smaller and
-          // closer together (more dense) than on mobile platforms.
           visualDensity: VisualDensity.adaptivePlatformDensity,
           brightness: Brightness.light,
         ),
@@ -49,6 +40,9 @@ class MyApp extends StatelessWidget {
           '/login': (_) => LoginScreen(),
           '/register': (_) => RegisterScreen(),
           '/add': (_) => AddScreen(),
+          '/home' : (_) => HomeScreen(),
+          '/test': (_) => TestScreen(),
+          '/settings': (_) => SettingsScreen(),
         },
         home: HomeScreen(),
         debugShowCheckedModeBanner: false,
